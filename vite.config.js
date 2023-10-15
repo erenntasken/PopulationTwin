783da71424aca9c506b8bddcda9854151b0d66e8
erentasken <eren.tasken@std.yeditepe.edu.tr>
2023-10-15 16:28:56 +0300

    deploy test 5

--- vite.config.js
+++ vite.config.js
@@ -3,19 +3,6 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   base: "/PopulationTwin/",
   build: {
-    commonjsOptions: {
-      include: [/node_modules/],
-    },
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          // Include the ./data directory in a chunk named 'data'
-          if (id.includes('/data/')) {
-            return 'data';
-          }
-        },
-      },
-    },
     assetsDir: 'assets', // Specify a directory for assets
   },
 });
